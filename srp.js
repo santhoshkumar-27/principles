04196cb27dba87bfa1a2e1079677c00cd1f8b561
santhoshkumar <its.santhoshkumar@outlook.com>
2023-09-25 19:35:03 +0530

    initial commit of SOLID principles

---
+++ srp.js
@@ -0,0 +1,38 @@
+// https://www.freecodecamp.org/news/solid-principles-for-programming-and-software-design/
+
+/**
+ * What is the Single-Responsibility Principle (SRP)?
+ * The Single-responsibility Principle, or SRP, states that a class should only have one reason to change. This means that a class should have only one job and do one thing.
+ * Let’s take a look at a proper example. You’ll always be tempted to put similar classes together – but unfortunately, this goes against the Single-responsibility principle. Why?
+ * The ValidatePerson object below has three methods: two validation methods, (ValidateName() and ValidateAge()), and a Display() method.
+ */
+class ValidatePerson {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    ValidateName(name) {
+        if (name.length > 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    ValidateAge(age) {
+        if (age > 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    Display() {
+        if (this.ValidateName(this.name) && this.ValidateAge(this.age)) {
+            console.log(`Name: ${this.name} and Age: ${this.age}`);
+        } else {
+            console.log('Invalid');
+        }
+    }
+}
